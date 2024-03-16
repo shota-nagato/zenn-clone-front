aba63175bceba59dba4f222e5597068453e7587b
shota-nagato <shota.nagato@gmail.com>
2024-03-16 19:23:30 +0900

    試しにテスト

---
+++ next/src/components/Button.tsx
@@ -0,0 +1,10 @@
+interface ButtonProps {
+  label: string
+  onClick: () => void
+}
+
+const Button = (props: ButtonProps) => {
+  return <button onClick={props.onClick}>{props.label}</button>
+}
+
+export default Button
